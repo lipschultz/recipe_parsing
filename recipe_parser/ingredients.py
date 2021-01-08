443132528258694ad0e0b428d158ba25a11d7a59
Michael Lipschultz <michael.lipschultz@gmail.com>
2021-01-08 06:37:58 -0500

    Organize units

--- recipe_parser/ingredients.py
+++ recipe_parser/ingredients.py
@@ -34,30 +34,41 @@ _units_length = [
 ]
 
 _units_amounts = [
-    'serving', 'servings',
-    'loaf', 'loaves',
-    'cookie', 'cookies',
     'ball', 'balls',
-    'piece', 'pieces',
-    'slice', 'slices',
-    'pie', 'pies',
-    'stick', 'sticks',
-    'package', 'packages', 'pkg', 'pkgs',
+    'block', 'blocks',
     'box', 'boxes',
+    'bunch', 'bunches',
     'can', 'cans',
-    'head', 'heads',
     'cloves', 'clove'
+    'cookie', 'cookies',
+    'drop', 'drops',
+    'dollop', 'dollops',
+    'head', 'heads',
+    'loaf', 'loaves',
+    'pack', 'packs',
+    'package', 'packages', 'pkg', 'pkgs',
+    'pie', 'pies',
+    'piece', 'pieces',
+    'roll', 'rolls',
+    'serving', 'servings',
+    'slice', 'slices',
+    'stalk', 'stalks',
+    'stick', 'sticks',
+]
+
+_unit_modifier_pre_amount = ['scant']
+
+_unit_modifier_pre_unit = [
     'medium', 'md', 'med',
     'large', 'lg',
     'small', 'sm',
-    'drop', 'drops',
-    'bunches', 'bunch',
-    'pack', 'packs',
-    'stalk', 'stalks',
-    'block', 'blocks',
-    'drop', 'drops',
-    'dollop', 'dollops'
+
+    'heaping',
+    'rounded',
+    'scant',
 ]
+
+_unit_modifier_post_unit = [r'\+']
 
 
 units = _units_weights + _units_volumes + _units_length + _units_amounts
